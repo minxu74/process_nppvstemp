76af26107fbad08c1d8f8d10011681f12b0cafd2
Min Xu <min.mxu@gmail.com>
2020-06-09 11:50:22 -0700

    change the order of writing results

--- proc.py
+++ proc.py
@@ -17,15 +17,15 @@ for gr in range(2):
                 fnvar = dirname + '/' + tr + '.NPP.sum.' + yr + '.nc'
                 fntmp = dirname + '/' + tr + '_' + yr + '_ann_temp.nc'
                 with nc4.Dataset(fnvar, 'r') as fv, nc4.Dataset(fntmp, 'r') as ft:
-                     pftstr = ''
+                     pftstr = '{:9.3f}'.format(ft.variables["TBOT"][gr])
                      for pft in range(fv.dimensions["pft"].size):
                          if fv.variables["pfts1d_ixy"][pft] == gr+1:
                             if fv.variables["NPP_sum"][pft] != fv.variables["NPP_sum"]._FillValue:
                                 # pfts1d_itype_veg
                                 # print (yr, tr, fv.variables["NPP_sum"][pft], ft.variables["TBOT"][0])
-                                pftstr+="{:9.3f} {:9.3f}".format(fv.variables["NPP_sum"][pft], ft.variables["TBOT"][gr])
-                                pass
-                fmtstr += pftstr
+                                pftstr+="{:9.3f}".format(fv.variables["NPP_sum"][pft], ft.variables["TBOT"][gr])
+                fw.write("{} {}\n".format(yr, pftstr))
+                #fmtstr += pftstr
             print (yr, tr, fmtstr)
-            fw.write("{} {}\n".format(yr, fmtstr))
+            #fw.write("{} {}\n".format(yr, fmtstr))
 
